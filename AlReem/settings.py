b4afb89ef5be0f94d2817d74902af050b96370a3
Parvati <parvathynair186@gmail.com>
2025-09-23 10:52:03 +0530

    'db-created'

--- AlReem/settings.py
+++ AlReem/settings.py
@@ -90,16 +90,24 @@ WSGI_APPLICATION = 'AlReem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'AlreemDB',
-        'USER':'postgres',
-        'PASSWORD':'admin',
-        'HOST':'localhost',
-        'PORT':5432
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'AlreemDB',
+#         'USER':'postgres',
+#         'PASSWORD':'admin',
+#         'HOST':'localhost',
+#         'PORT':5432
 
-    }
+#     }
+# }
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
